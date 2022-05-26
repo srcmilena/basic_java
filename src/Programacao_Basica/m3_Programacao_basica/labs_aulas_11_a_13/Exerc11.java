19127665111bf0d3aee055984d90beeec3db9d5b
srcmilena <87768754+srcmilena@users.noreply.github.com>
2022-05-25 23:26:49 -0300

    exercicio 11 (aulas 11, 12, 13)

--- src/Programacao_Basica/m3_Programacao_basica/labs_aulas_11_a_13/Exerc11.java
+++ src/Programacao_Basica/m3_Programacao_basica/labs_aulas_11_a_13/Exerc11.java
@@ -8,10 +8,28 @@
 
 package programacao_basica.m3_programacao_basica.labs_aulas_11_a_13;
 
+import java.util.Scanner;
+
 public class Exerc11 {
 
     public static void main(String[] args) {
 
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Digite o primeiro número (inteiro): ");
+    int primeiroNum = scan.nextInt();
 
+    System.out.print("Digite o segundo número (inteiro): ");
+    int segundoNum = scan.nextInt();
+
+    System.out.print("Digite o terceiro número (real): ");
+    double terceiroNum = scan.nextDouble();
+
+    double produto = (2*primeiroNum)+(segundoNum/2);
+    double soma = (3*primeiroNum) + terceiroNum;
+    double aoCubo = terceiroNum*terceiroNum*terceiroNum;
+
+    System.out.println("O produto do dobro do primeiro com metade do segundo é: " + produto);
+    System.out.println("A soma do triplo do primeiro com o terceiro é: " + soma);
+    System.out.println("O terceiro elevado ao cubo é: " + aoCubo);
     }
 }
